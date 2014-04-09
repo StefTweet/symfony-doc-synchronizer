183cf1b400adc4dcccc4055219d9b039e03f5fe1
Julien DIDIER <julien@didier.io>
2014-04-09 12:15:39 +0200

    skelton of DocumentParser

---
+++ src/Symfony/DocSynchronizerBundle/Parser/DocumentParser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Symfony\DocSynchronizerBundle\Parser;
+
+class DocumentParser
+{
+    public function parse($text)
+    {
+        return array();
+    }
+}
--- src/Symfony/DocSynchronizerBundle/Tests/Parser/DocumentParserTest.php
+++ src/Symfony/DocSynchronizerBundle/Tests/Parser/DocumentParserTest.php
@@ -2,9 +2,15 @@
 
 namespace Symfony\DocSynchronizerBundle\Tests\Parser;
 
+use Symfony\DocSynchronizerBundle\Parser\DocumentParser;
+
 class DocumentParserTest extends \PHPUnit_Framework_TestCase
 {
-    public function testEmpty()
+    public function testParseEmptyDocument()
     {
+        $parser = new DocumentParser();
+        $ast = $parser->parse('');
+
+        $this->assertSame(array(), $ast, 'AST from empty document should be empty');
     }
 }
