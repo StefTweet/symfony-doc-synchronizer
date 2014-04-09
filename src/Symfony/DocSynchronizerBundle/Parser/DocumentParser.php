106443adb60ff8f92575f5fdf8a677a87b682fb7
Julien DIDIER <julien@didier.io>
2014-04-09 12:36:18 +0200

    working on DocumentParser

--- src/Symfony/DocSynchronizerBundle/Parser/DocumentParser.php
+++ src/Symfony/DocSynchronizerBundle/Parser/DocumentParser.php
@@ -4,8 +4,39 @@ namespace Symfony\DocSynchronizerBundle\Parser;
 
 class DocumentParser
 {
+    CONST LEVEL_H1 = '=';
+    CONST LEVEL_H2 = '-';
+    CONST LEVEL_H3 = '`';
+    const LEVEL_H4 = ':';
+    const LEVEL_H5 = "'";
+
     public function parse($text)
     {
-        return array();
+        $ast = array();
+
+        $previousLine = '';
+        $h1 = null;
+        $h2 = null;
+        $h3 = null;
+        foreach(explode(PHP_EOL, $text) as $lineNumber => $line){
+            if (isset($line[0]) && self::LEVEL_H1 === $line[0]) {
+                $ast[$previousLine] = array();
+                $h1 = $previousLine;
+            }
+
+            if (isset($line[0]) && self::LEVEL_H2 === $line[0]) {
+                $ast[$h1][$previousLine] = array();
+                $h2 = $previousLine;
+            }
+
+            if (isset($line[0]) && self::LEVEL_H3 === $line[0]) {
+                $ast[$h1][$h2][$previousLine] = array();
+                $h3 = $previousLine;
+            }
+
+            $previousLine = $line;
+        } 
+
+        return $ast;
     }
 }
--- src/Symfony/DocSynchronizerBundle/Tests/Parser/DocumentParserTest.php
+++ src/Symfony/DocSynchronizerBundle/Tests/Parser/DocumentParserTest.php
@@ -13,4 +13,39 @@ class DocumentParserTest extends \PHPUnit_Framework_TestCase
 
         $this->assertSame(array(), $ast, 'AST from empty document should be empty');
     }
+
+    public function testParseDocument()
+    {
+        $parser = new DocumentParser();
+        $ast = $parser->parse("H1
+==
+
+Paragraph
+
+H2
+--
+
+H3
+``
+
+Paragraph
+
+H4
+::
+
+Paragraph
+
+H5
+''
+
+Paragraph");
+
+        $this->assertSame(array(
+            'H1' => array(
+                'H2' => array(
+                    'H3' => array()
+                )
+            )
+        ), $ast, 'AST must have one head title and one pagraph title');
+    }
 }
